40e9b22ef88342911038dc45728c2768cb2d7417
qamarularifin <qamarulafro89@gmail.com>
2023-10-10 22:19:26 +0800

    Added data

---
+++ EnquiryAppStatusApi/Data/EnquiryAPIDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PermitApplicationWebApi.Models;
+
+namespace EnquiryAppStatusApi.Data
+{
+    public class EnquiryAPIDbContext : DbContext
+    {
+
+        public EnquiryAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Enquiry> Enquiries { get; set; }
+        public DbSet<Permit> Permits { get; set; }
+        
+    }
+}
