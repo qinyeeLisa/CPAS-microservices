57e1c5a8366f60cba970cee9bf5119faf8adc174
QIN YEE LIM <limqinyeelisa@gmail.com>
2023-10-15 23:56:52 +0800

    Added send email function

---
+++ UserWebApi/Services/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace UserWebApi.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string userEmail, string subject, string message)
+        {
+            var sender = "projecttestmail314@gmail.com";
+            var pwd = "qrxi juot yepu ccyh";
+
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(sender, pwd)
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(from: sender,
+                                to: userEmail,
+                                subject,
+                                message
+                                ));
+        }
+    }
+}
