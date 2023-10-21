0fbd974aeb5decf6845972d20c290bd70ad52a65
QIN YEE LIM <limqinyeelisa@gmail.com>
2023-10-21 17:06:54 +0800

    Added unit test

---
+++ UserWebApi.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using UserWebApi.Controllers;
+
+namespace UserWebApi.Test
+{
+    public class UnitTest1
+    {
+
+
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            //var userController = new UserController()
+            //string str2 = " ";
+            //string str3 = "World!";
+
+            ////Act 
+            //string result = string.Concat(str1, str2, str3);
+
+            ////Assert
+            //Assert.Equal("Hello World!", result);
+        }
+    }
+}
